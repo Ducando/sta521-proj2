ecc08fef30055c53c55f2e17a0ede011ecd62c6b
Alison-R <alisonlreynolds24@gmail.com>
2021-10-26 14:35:23 -0400

    Base CVmaster function

--- CVmaster.R
+++ CVmaster.R
@@ -1,8 +1,14 @@
-CVmaster <- function(model,X,y,k,loss){
-  library(caret)
+CVmaster <- function(model=c("lda","qda","rf"),X,y,k,loss=c("class_accur")){
+  require(caret)
   folds <- createFolds(y,k)
   
   crossVal <- rep(NA,k)
   for (i in 1:k){
+    mod <- train(x=X[-folds[[i]],],y=y[-folds[[i]]],method = model)
+    preds <- predict(mod,X[folds[[i]],])
+    if (loss == "class_accur"){
+      crossVal[i] <- mean(preds != y[folds[[i]]])
+    }
   }
+  return(mean(crossVal))
 }
