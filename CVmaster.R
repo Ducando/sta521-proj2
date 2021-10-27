3fee3b5427065ac64fc98fa647e8e6a2863cb712
Alison-R <alisonlreynolds24@gmail.com>
2021-10-26 22:41:57 -0400

    Added loss function

--- CVmaster.R
+++ CVmaster.R
@@ -1,12 +1,20 @@
-CVmaster <- function(model=c("lda","qda","rf"),X,y,k,loss=c("class_accur")){
+CVmaster <- function(model=c("lda","qda","rf","logistic","svmLinear"),X,y,k,loss=c("accuracy","error")){
   require(caret)
   folds <- createFolds(y,k)
   
   crossVal <- rep(NA,k)
   for (i in 1:k){
+    if (model == "logistic"){
+      mod <- train(x=X[-folds[[i]],],y=y[-folds[[i]]],method = "glmnet",family="binomial")
+    }
+    else{
     mod <- train(x=X[-folds[[i]],],y=y[-folds[[i]]],method = model)
+    }
     preds <- predict(mod,X[folds[[i]],])
-    if (loss == "class_accur"){
+    if (loss == "accuracy"){
+      crossVal[i] <- mean(preds == y[folds[[i]]])
+    }
+    else if (loss == "error"){
       crossVal[i] <- mean(preds != y[folds[[i]]])
     }
   }
