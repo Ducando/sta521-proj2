ffa5334d14fa97831c75b0b9df4d8e41990ab7c4
Jackie Du <jacquelyndu8@gmail.com>
2021-11-12 21:42:05 -0500

    finish log, rf, boost, and add structure for accuracies and roc

--- CVmaster2.R
+++ CVmaster2.R
@@ -1,4 +1,4 @@
-CVmaster2 <- function (model=c("lda","qda","rf","logistic","svmLinear"),X,y,k,loss, ntree=NA) {
+CVmaster2 <- function (model,X,y,k,loss, ntree=NA) {
   require(tidyverse)
   require(tidymodels)
   require(workflows)
@@ -6,8 +6,15 @@ CVmaster2 <- function (model=c("lda","qda","rf","logistic","svmLinear"),X,y,k,lo
   require(tibble)
   # require(caret)
   require(rsample)
+  require(xgboost)
   # require(parallel)
   
+  # check if model in one of the options 
+  model_options <- c("lda","qda","rf","logistic","svm", "rf_boost")
+  if(!(model %in% model_options)){
+    message(paste0("Model not available, please choose from: lda, qda, rf, logistic, svm, rf_boost"))
+    break
+  }
   
   # split data into folds 
   data <- bind_cols(X, labels = y)
@@ -42,6 +49,8 @@ CVmaster2 <- function (model=c("lda","qda","rf","logistic","svmLinear"),X,y,k,lo
                 control = control_grid(save_pred = TRUE),
                 resamples = folds)
     
+    return(res)
+    
     
     ## FIND ACCURACIES
     # find best parameter based on accuracy 
@@ -62,7 +71,8 @@ CVmaster2 <- function (model=c("lda","qda","rf","logistic","svmLinear"),X,y,k,lo
         group_by(id) %>%
         mutate(mean = mean(estimate)) %>%
         select(id, penalty, metric, mean) %>%
-        distinct() -> accuracies
+        distinct() %>%
+        mutate(model = "Logistic Regression") -> accuracies
       
       # append to estimates
       estimates <- c(estimates, accuracy = list(accuracies))
@@ -94,6 +104,7 @@ CVmaster2 <- function (model=c("lda","qda","rf","logistic","svmLinear"),X,y,k,lo
       add_model(mod) %>%
       add_formula(labels ~.)
     
+    ## CHANGE TO 25 TO RUN AT END
     res <- workflow %>%
       tune_grid(grid = 5, 
                 control = control_grid(save_pred = TRUE),
@@ -105,17 +116,20 @@ CVmaster2 <- function (model=c("lda","qda","rf","logistic","svmLinear"),X,y,k,lo
       
       res %>%
         select(id, .metrics) %>%
-        hoist(.metrics, min_n = "min_n", metric = ".metric", estimate = ".estimate") %>%
+        hoist(.metrics, mtry = "mtry", min_n = "min_n", metric = ".metric", estimate = ".estimate") %>%
         select(-.metrics) %>%
         unnest_longer(min_n) %>%
         filter(min_n == best$min_n) %>%
+        unnest_longer(mtry) %>%
+        filter(mtry == best$mtry) %>%
         unnest_longer(metric) %>%
         filter(metric == "accuracy") %>%
         unnest_longer(estimate) %>%
         group_by(id) %>%
         mutate(mean = mean(estimate)) %>%
-        select(id, min_n, metric, mean) %>%
-        distinct() -> accuracies
+        select(id, mtry, min_n, metric, mean) %>%
+        distinct() %>%
+        mutate(model = "Random Forest") -> accuracies
       
       # append to estimates
       estimates <- c(estimates, accuracy = list(accuracies))
@@ -136,16 +150,79 @@ CVmaster2 <- function (model=c("lda","qda","rf","logistic","svmLinear"),X,y,k,lo
     
   } # end random forest 
   else if (model == "lda") {
+
+
     
   } # end lda 
   else if (model == "qda") {
     
   } # end qda 
+  else if (model == "rf_boost"){
+    
+    # add more arguments?
+    mod <- boost_tree(mtry = tune(), min_n = tune(), trees = 5) %>% 
+      set_mode("classification") %>% 
+      set_engine("xgboost")
+    
+    workflow <- workflow() %>%
+      add_model(mod) %>%
+      add_formula(labels ~.)
+    
+    ## CHANGE TO 25 TO RUN AT END
+    res <- workflow %>%
+      tune_grid(grid = 5, 
+                control = control_grid(save_pred = TRUE),
+                resamples = folds)
+    
+    # if accuracy
+    if("accuracy" %in% loss){
+      res %>% select_best("accuracy")  -> best
+
+      res %>%
+        select(id, .metrics) %>%
+        hoist(.metrics, mtry = "mtry", min_n = "min_n", metric = ".metric", estimate = ".estimate") %>%
+        select(-.metrics) %>%
+        unnest_longer(min_n) %>%
+        filter(min_n == best$min_n) %>%
+        unnest_longer(mtry) %>%
+        filter(mtry == best$mtry) %>%
+        unnest_longer(metric) %>%
+        filter(metric == "accuracy") %>%
+        unnest_longer(estimate) %>%
+        group_by(id) %>%
+        mutate(mean = mean(estimate)) %>%
+        select(id, mtry, min_n, metric, mean) %>%
+        distinct() %>%
+        mutate(model = "Boosted Trees") -> accuracies
+
+      # append to estimates
+      estimates <- c(estimates, accuracy = list(accuracies))
+    } # end accuracies
+
+    # if roc
+    if("roc" %in% loss){
+      res %>%
+        collect_predictions(parameters = best) %>%
+        roc_curve(labels, `.pred_-1`) %>%
+        mutate(model = "Boosted Trees") -> roc
+
+      # append to estimates
+      estimates <- c(estimates, roc = list(roc))
+
+    } # end roc
+    
+    
+    
+    
+    
+    
+    
+  } # end boost 
   else {
     
   } # end svm 
   
-  return(estimates)
+  # return(estimates)
 }
 
 
